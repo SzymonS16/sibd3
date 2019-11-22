741c23a59006be554ab7b4581944f19f6cc6a706
SzymonS16 <szymon1965@o2.pl>
2019-11-22 11:39:41 +0000

    st

---
+++ new_client.php
@@ -0,0 +1,15 @@
+<html>
+ <body>
+ <form action="insert_client.php" method="post">
+ <h3>Insert a new client</h3>
+    <p>VAT: <input type="text" name="VAT"/></p>
+    <p>name: <input type="text" name="name"/></p>
+    <p>birth_date: <input type="text" name="birth_date"/></p>
+    <p>street: <input type="text" name="street"/></p>
+    <p>city: <input type="text" name="city"/></p>
+    <p>zip: <input type="text" name="zip"/></p>
+    <p>gender: <input type="text" name=""/></p>
+    <p><input type="submit" value="Submit"/></p>
+ </form>
+ </body>
+</html>
