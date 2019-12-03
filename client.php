60c46e7f6039307bfa3cee24cc2ec90fbb6aaf29
SzymonS16 <szymon1965@o2.pl>
2019-12-03 20:58:19 +0000

    gvh

---
+++ client.php
@@ -0,0 +1,131 @@
+<html>
+ <body>
+ <h3>Clients</h3>
+
+ <form name="app" action="" method="post">
+ <p>vat: <input type="text" name="vat"/></p>
+ <p>name: <input type="text" name="name"/></p>
+ <p>street: <input type="text" name="street"/></p>
+ <p>city: <input type="text" name="city"/></p>
+ <p>zip: <input type="text" name="zip"/></p>
+ <p><input type="submit" value="Submit"/></p>
+ </form>
+
+<?php
+    $host = "db.tecnico.ulisboa.pt";
+    $user = "ist195137";
+    $pass = "taec8707";
+    $dsn = "mysql:host=$host;dbname=$user";
+    try
+    {
+    $connection = new PDO($dsn, $user, $pass);
+    }
+    catch(PDOException $exception)
+    {
+    echo("<p>Error: ");
+    echo($exception->getMessage());
+    echo("</p>");
+    exit();
+    }
+
+    if((empty($_REQUEST['vat']))
+        && (empty($_REQUEST['name']))
+        && (empty($_REQUEST['street']))
+        && (empty($_REQUEST['city']))
+        && (empty($_REQUEST['zip'])))
+    {
+        $sql = "SELECT * FROM client"; 
+    }
+
+    else{
+        $vat = $_REQUEST['vat'];
+        $name = $_REQUEST['name'];
+        $street = $_REQUEST['street'];
+        $city = $_REQUEST['city'];
+        $zip = $_REQUEST['zip'];
+    
+        $where = "WHERE ";
+        $first = TRUE;
+    
+        if(!empty($vat)){
+            $where .= "vat = '$vat'";
+            $first = FALSE;
+        }
+    
+        if(!empty($name)){
+            if($first){
+                $where .= " name LIKE '%$name%'";
+                $first = FALSE;
+            }
+            else{
+                $where .= " and name LIKE '%$name%'"; 
+            }
+        }
+    
+        if(!empty($street)){
+            if($first){
+                $where .= " street LIKE '%$street%'";
+                $first = FALSE;
+            }
+            else{
+                $where .= " and street LIKE '%$street%'";
+            }       
+        }
+    
+        if(!empty($city)){
+            if($first){
+                $where .= " city LIKE '%$city%'";
+                $first = FALSE;
+            }
+            else{
+                $where .= " and city LIKE '%$city%'";
+            }
+        }
+    
+        if(!empty($zip)){
+            if($first){
+                $where .= " zip LIKE '%$zip%'";
+                $first = FALSE;
+            }
+            else{
+                $where .= " and zip LIKE '%$zip%'";
+            }
+        }
+        
+        $sql = "SELECT * FROM client $where"; 
+    }
+    
+    $result = $connection->query($sql);
+    if ($result == FALSE)
+    {
+    $info = $connection->errorInfo();
+    echo("<p>Error: {$info[2]}</p>");
+    exit();
+    }
+    echo("<table border=\"0\" cellspacing=\"5\">\n");
+    
+    if($result -> rowCount() > 0){
+        foreach($result as $row)
+        {
+        echo("<tr>\n");
+        echo("<td>{$row['vat']}</td>\n");
+        echo("<td>{$row['name']}</td>\n");
+        echo("<td><a href=\"client_history.php?vat=");
+        echo($row['vat']);
+        echo("\">History</a></td>\n");
+        echo("</tr>\n");
+        }
+        echo("</table>\n");
+        $connection = null;
+    }
+    else{
+        echo("<p>Client not found!</p>");
+        echo("<p><a href=\"new_client.php");
+        echo("\">Create new client</a></p>\n");
+    }  
+?>
+ </body>
+</html>
+
+
+
---
+++ client_history.php
@@ -0,0 +1,93 @@
+<?php 
+    if(!empty($_REQUEST['vat'])){
+        
+        $cl = $_REQUEST['vat'];
+
+        $host = "db.tecnico.ulisboa.pt";
+        $user = "ist195137";
+        $pass = "taec8707";
+        $dsn = "mysql:host=$host;dbname=$user";
+        try
+        {
+        $connection = new PDO($dsn, $user, $pass);
+        }
+        catch(PDOException $exception)
+        {
+        echo("<p>Error: ");
+        echo($exception->getMessage());
+        echo("</p>");
+        exit();
+        }
+        
+        $sql = "SELECT * FROM appointment WHERE vat_client = '$cl'";
+
+        $result = $connection->query($sql);
+        if ($result == FALSE)
+        {
+        $info = $connection->errorInfo();
+        echo("<p>Error: {$info[2]}</p>");
+        exit();
+        }
+        
+        echo("<h1>Appointment history</h1>");
+
+        if($result -> rowCount() > 0){
+            $i = 0;
+            echo("<table border=\"0\" cellspacing=\"5\">\n");
+            foreach($result as $row)
+            {
+            echo("<tr>\n");
+            echo("<td>{$row['vat_doctor']}</td>\n");
+            echo("<td>{$row['date_timestamp']}</td>\n");
+            echo("<td>{$row['description']}</td>\n");
+            echo("<td>{$row['vat_client']}</td>\n");
+            echo("</tr>\n");
+
+            $doctor[$i] = $row['vat_doctor'];
+            $time[$i] = $row['date_timestamp'];
+            $i = $i + 1;
+            }
+            echo("</table>\n");
+
+            echo("<h1>Conslutation history</h1>");
+            $cons_set = FALSE;
+
+            for($x = 0; $x < sizeof($doctor); $x++){
+
+                $sqll = "SELECT * FROM consultation WHERE vat_doctor = '$doctor[$x]' AND date_timestamp = '$time[$x]'";
+                $cons = $connection->query($sqll);
+                
+                if ($cons == FALSE)
+                {
+                $info = $connection->errorInfo();
+                echo("<p>Error: {$info[2]}</p>");
+                exit();       
+                }
+                
+                if($cons -> rowCount() > 0){
+                    $cons_set = TRUE;
+                    echo("<table border=\"0\" cellspacing=\"5\">\n");
+                    foreach($cons as $roww)
+                    {
+                    echo("<tr>\n");
+                    echo("<td>{$roww['vat_docotr']}</td>\n");
+                    echo("<td>{$roww['date_timestamp']}</td>\n");
+                    echo("<td>{$roww['soap_s']}</td>\n");
+                    echo("<td>{$roww['soap_o']}</td>\n");
+                    echo("<td>{$roww['soap_a']}</td>\n");
+                    echo("<td>{$roww['soap_p']}</td>\n");
+                    echo("</tr>\n");
+                    }
+                    echo("</table>\n");
+                }
+                if(!$cons_set){
+                    echo("<p>No records in consultations history</p>");
+                }
+            }
+            $connection = null; 
+        }
+		else{
+			echo("<p>No records in appointments history</p>");
+        }
+}
+?>
