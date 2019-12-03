6af13ed32718076a843f66e5e29c122a0a05556c
SzymonS16 <szymon1965@o2.pl>
2019-12-03 22:25:50 +0000

    savfs

--- client_history.php
+++ client_history.php
@@ -19,7 +19,7 @@
         exit();
         }
         
-        $sql = "SELECT * FROM appointment WHERE vat_client = '$cl'";
+        $sql = "SELECT * FROM appointment WHERE vat_client = '$cl' ORDER BY date_timestamp DESC";
 
         $result = $connection->query($sql);
         if ($result == FALSE)
@@ -34,6 +34,12 @@
         if($result -> rowCount() > 0){
             $i = 0;
             echo("<table border=\"0\" cellspacing=\"5\">\n");
+            echo("<tr>\n");
+            echo("<th>vat_doctor</th>\n");
+            echo("<th>date_timestamp</th>\n");
+            echo("<th>description</th>\n");
+            echo("<th>vat_client</th>\n");
+            echo("</tr>\n");
             foreach($result as $row)
             {
             echo("<tr>\n");
@@ -50,11 +56,21 @@
             echo("</table>\n");
 
             echo("<h1>Conslutation history</h1>");
+            echo("<table border=\"0\" cellspacing=\"5\">\n");
+            echo("<tr>\n");
+            echo("<th>vat_doctor</th>\n");
+            echo("<th>date_timestamp</th>\n");
+            echo("<th>soap_s</th>\n");
+            echo("<th>soap_o</th>\n");
+            echo("<th>soap_a</th>\n");
+            echo("<th>soap_p</th>\n");
+            echo("</tr>\n");
             $cons_set = FALSE;
 
             for($x = 0; $x < sizeof($doctor); $x++){
-
-                $sqll = "SELECT * FROM consultation WHERE vat_doctor = '$doctor[$x]' AND date_timestamp = '$time[$x]'";
+                $sqll = "SELECT * FROM consultation WHERE vat_doctor = '$doctor[$x]' 
+                AND date_timestamp = '$time[$x]'
+                ORDER BY date_timestamp";
                 $cons = $connection->query($sqll);
                 
                 if ($cons == FALSE)
@@ -66,24 +82,26 @@
                 
                 if($cons -> rowCount() > 0){
                     $cons_set = TRUE;
-                    echo("<table border=\"0\" cellspacing=\"5\">\n");
                     foreach($cons as $roww)
                     {
                     echo("<tr>\n");
-                    echo("<td>{$roww['vat_docotr']}</td>\n");
+                    echo("<td>{$roww['vat_doctor']}</td>\n");
                     echo("<td>{$roww['date_timestamp']}</td>\n");
                     echo("<td>{$roww['soap_s']}</td>\n");
                     echo("<td>{$roww['soap_o']}</td>\n");
                     echo("<td>{$roww['soap_a']}</td>\n");
                     echo("<td>{$roww['soap_p']}</td>\n");
                     echo("</tr>\n");
-                    }
-                    echo("</table>\n");
-                }
-                if(!$cons_set){
-                    echo("<p>No records in consultations history</p>");
+                    }  
                 }
             }
+
+            echo("</table>\n");
+
+            if(!$cons_set){
+                echo("<p>No records in consultations history</p>");
+            }
+
             $connection = null; 
         }
 		else{
