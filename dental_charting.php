58ff55d3526b3efbecc2e0b24b18d2fb73ffb225
SzymonS16 <szymon1965@o2.pl>
2019-12-04 15:29:18 +0000

    sa

---
+++ dental_charting.php
@@ -0,0 +1,14 @@
+<html>
+ <body>
+ <form action="insert_dental_charting.php" method="post">
+ <h3>Dental charting</h3>
+    <p>upper right 1: <input type="text" name="vat"/> <input type="number" step="0.01" name=""></p>
+    <p>name: <input type="text" name="name"/></p>
+    <p>street: <input type="text" name="street"/></p>
+    <p>city: <input type="text" name="city"/></p>
+    <p>zip: <input type="text" name="zip"/></p>
+ <p><input type="submit" value="Submit"/></p>
+ </form>
+ <h1><a href="new_client.php">Create new client</a></h1>
+ </body>
+</html>
